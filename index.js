cbc4a2785f27c9be131a481b546ecbf83fffc7ff
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-03-29 14:20:15 -0400

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const resolve = (res, status, message) => {
+  console.log({
+    status, message
+  })
+  res.status(parseInt(status)).json({
+    message
+  });
+}
+
+app.post('/', (req, res) => {
+  const status = req.get('Authorization');
+
+  if (status) {
+    resolve(res, status, `Status changed to ${status}`)
+  } else {
+    resolve(res, 200, 'OK')
+  }
+});
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
